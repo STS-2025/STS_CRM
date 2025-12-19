a861f305526a6a8241bf071dc3b1016b55cd5075
Micheal Mithra <mmichealmithra@gmail.com>
2025-12-19 13:04:19 +0530

    CHANGE GREEN

--- api/db.php
+++ api/db.php
@@ -1,9 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "crm";
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db = "crm";
 
+$host = "192.168.29.26";
+$user = "mariselvam";
+$pass = "mari@123";
+$db = "crm";
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
